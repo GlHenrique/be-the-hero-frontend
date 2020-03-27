9953e2a48b64573e29a75ebea507ea0ab519a8a1
Glenio Carvalho <gleniohenrique-carvalho@hotmail.com>
2020-03-27 10:43:43 -0300

    Layout finished

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -14,7 +14,7 @@ export default function Register() {
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos de sua ONG.</p>
                     <Link className='back-link' to="/">
                         <FiArrowLeft size={16} color="#E02041"/>
-                        Não tenho registro
+                        Tenho registro
                     </Link>
                 </section>
                 <form>
